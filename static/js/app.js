208699308e8769c1a71b10f66bc165a9b2dfe441
Janel C <janel.alm@gmail.com>
2018-12-18 11:40:04 -0800

    working on filtering process

--- static/js/app.js
+++ static/js/app.js
@@ -1,53 +1,122 @@
 // from data.js
 var tableData = data;
 
-// Creating a function which will create table on webpage
+// Create <thead>
+// Create <thead>
+var thead = d3.select("thead");
+var row =thead.append("tr")
+var headNames = ["Date", "City", "State", "Country", "Shape", "Duration", "Comments"]
 
-function createTableHead() {
-    // get the reference for the table area
-    var tablearea = document.getElementById("table-area");
-
-    // creates a <table> element
-    var tbl = document.createElement("table");
-    tbl.setAttribute("id", "ufo-table");
-    
-    // creates <thead> 
-    var tblHead = document.createElement("thead");
-    var tr = document.createElement("tr");
-
-    var headNames = ["Date", "City", "State", "Country", "Shape", "Duration", "Comments"]
-
-    // create <th> elemnent for each element in headNames Array
-    for (var i = 0; i < headNames.length; i++){
-        var name = headNames[i];
-        var th = document.createElement('th');
-        th.appendChild(document.createTextNode(name))
-    }
-
-    tr.appendChild(th)
-    tblHead.appendChild(tr)
-    document.getElementById("ufo-table").appendChild(tblHead);
+for (var i = 0; i < headNames.length; i++){
+    var name = headNames[i];
+    var cell = row.append('th');
+    cell.attr("class", "table-head");
+    cell.text(name); 
 }
 
-function createTableBody(){
-    // get the reference for the table area
-    var tablearea = document.getElementById("ufo-table");
+// Select <tbody>
+// Create <tbody>
+var tbody = d3.select("tbody");
+tableData.forEach((ufoReport) => {
+  var row = tbody.append("tr");
+  row.attr("class","table-primary");
+  Object.entries(ufoReport).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
 
-     // creates <tbody> 
-    var tblBody = document.createElement("tbody");
-    // create <tr> element for each element in array tableData
-    for ( var j=0; j < tableData.length; j++){
-        var tableDataElement = tableData[i];
-        var tr = document.createElement("tr");
-        // create <td> element for each element
-        for (var k =0; k < tabldDataElement.length; k++) {
-        var tableDataInside = tableDataElement[j]
-        var td = document.createElement('td');
-        td.appendChild(document.createTextNode(Object.values(tableDataInside)))
+function myFunction0() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("datetime");
+    table = document.getElementById("ufo-table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(input) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
         }
-    tr.appendChild(td)
-    tblBody.appendChild(tr)
-    document.getElementById("ufo-table").appendChild(tblBody);
+      }       
     }
+};
 
-// YOUR CODE HERE!
+
+function myFunction1() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("city");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("ufo-table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+};
+
+function myFunction2() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("state");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("ufo-table");
+        tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }       
+    }
+};
+
+function myFunction3() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("state");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("ufo-table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[3];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }       
+    }
+};
+
+function myFunction4() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("shape");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("ufo-table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[4];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }       
+    }
+};
