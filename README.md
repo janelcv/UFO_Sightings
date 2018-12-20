1c61f1e290c5ed6546c8d8898c6396406da8b41c
Janel C <janel.alm@gmail.com>
2018-12-20 10:57:56 -0800

    my two versions

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # UFO_Sightings
+
+### STILL Don't Believe In ALIENS? Watch This And Think Again!
+#### Description
+
+[UFO Sightings]() is a project made to display the evidences of the UFO object noticed by people all over US in different state during the year  2010. 
+
+#### Methodology
+Project has two version:
+1. In the first version [onkeyup](https://www.w3schools.com/jsref/event_onkeyup.asp) event was used to filter the data. This version is stored in file [app.js]() and [index.html]()
+>Data only filtered by one filter each time.
+2. Second version stored in [app2.js]() and [index2.html]()
+
+1. ###HTML creation.
+
+[Dataset]() was presented as an array of objects and stored as a `data.js`. 
+
---
+++ static/js/app2.js
@@ -0,0 +1,71 @@
+// from data.js
+var tableData = data;
+
+// Create <thead>
+// Create <thead>
+var thead = d3.select("thead");
+var row =thead.append("tr")
+var headNames = ["Date", "City", "State", "Country", "Shape", "Duration", "Comments"]
+
+
+for (var i = 0; i < headNames.length; i++){
+    var name = headNames[i];
+    var cell = row.append('th');
+    cell.attr("class", "table-head");
+    cell.text(name); 
+}
+
+// Select <tbody>
+// Create <tbody>
+
+function createTable(row){
+var tbody = d3.select("tbody");
+row.forEach((ufoReport) => {
+  var row = tbody.append("tr");
+  row.attr("class","table-primary");
+  Object.entries(ufoReport).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+};
+createTable(tableData)
+
+
+
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  var inputTbody = d3.select("tbody");
+  var loadFiltData = "n";
+
+  // Select the input element and get the raw HTML node
+  var inputDate = d3.select("#datetime"); 
+  var inputCity= d3.select("#city");
+  var inputState = d3.select("#state");
+  var inputCountry = d3.select("#country")
+  var inputShape   = d3.select("#shape")
+
+  // Get the value property of the input element
+  var dateValue = inputDate.property("value");
+  var cityValue = inputCity.property("value");
+  var stateValue = inputState.property("value");
+  var countryValue = inputCountry.property("value");
+  var shapeValue = inputShape.property("value");
+
+  console.log(dateValue);
+  console.log(stateValue);
+  console.log(cityValue);
+  console.log(countryValue);
+  console.log(shapeValue);
+
+  var filteredData = tableData.filter(ufo => (ufo.datetime === dateValue) && (ufo.state === stateValue));
+
+  console.log(filteredData);
+  // //Clear all previuos data from UFO table
+  inputTbody.html("");
+  createTable(filteredData)
+});
